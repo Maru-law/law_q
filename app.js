ad8e03367792aa97cbeb28605fd3ffc3d633e039
Maru-law <taku.0513.taka@gmail.com>
2026-07-03 17:34:13 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// ↓GASのウェブアプリURLを再設定してください
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwFoCjDdLK2duXq8Hav_rjYHp6J8oFVkNvhZY914oDe-tQuC2BUqAlHEOe1qRUpzQdMSw/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby71xzQ4WDcpNDh677BY5hZdNeiM8Y3kpo4w-2jZA81tUyFmGvr2lqIZ6DDPcSoylWFZw/exec';
 
 let allQuestions = [];
 let maxIncorrect = 0;
